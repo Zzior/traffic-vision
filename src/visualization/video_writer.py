bcb09f4128bec6c3944e99c229364ec170f00e1b
Zzior <xasan10102020@gmail.com>
2026-02-22 18:54:45 +0500

    📹 Update VideoWriter

--- src/visualization/video_writer.py
+++ src/visualization/video_writer.py
@@ -11,8 +11,7 @@ class VideoWriter:
         self.fps = self.config["fps"]
         self.fourcc = self.config["fourcc"]
         self.skip_frames = self.config["skip_frames"]
-        self.resolution = config["show"]["output_size"]
-        self.segment_size = self.config["segment_size"]  # секунды
+        self.segment_size = self.config["segment_size"]
 
         output_path = Path(self.config["output_path"]).expanduser()
         self.output_path = output_path if output_path.is_absolute() else project_dir / output_path
@@ -22,16 +21,21 @@ class VideoWriter:
         self.total_frames_processed = 0
 
         self.segment_frames = int(self.fps * self.segment_size)
-        self.writer = self.create_new_writer(filepath)
+
+        self.video_path = filepath
+        self.writer: cv2.VideoWriter | None = None
 
 
-    def create_new_writer(self, filepath: str = None) -> cv2.VideoWriter:
-        if not filepath:
+    def create_new_writer(self, width: int, height: int) -> cv2.VideoWriter:
+        if self.video_path:
+            filepath = self.video_path
+
+        else:
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
             filepath = self.output_path / f"{timestamp}.mkv"
 
         fourcc = cv2.VideoWriter_fourcc(*self.fourcc)  # noqa
-        writer = cv2.VideoWriter(str(filepath), fourcc, self.fps, self.resolution)
+        writer = cv2.VideoWriter(str(filepath), fourcc, self.fps, (width, height))
         return writer
 
     def process(self, frame: np.ndarray) -> None:
@@ -42,13 +46,15 @@ class VideoWriter:
         if (self.total_frames_processed % self.skip_frames) != 0:
             return
 
+        if not self.writer:
+            self.writer = self.create_new_writer(frame.shape[1], frame.shape[0])
+
         self.writer.write(frame)
         self.frames_in_segment += 1
 
         if self.frames_in_segment >= self.segment_frames:
             self.close_current_writer()
             self.frames_in_segment = 0
-            self.writer = self.create_new_writer()
 
     def close_current_writer(self):
         if self.writer is not None:
